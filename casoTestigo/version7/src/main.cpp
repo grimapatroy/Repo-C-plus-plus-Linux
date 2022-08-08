d74ffd01112142c6a115ecb53c18c4d9a7aa2f3d
grimapaj <lenin.coste@gmail.com>
2022-08-07 22:10:25 -0500

    media aparte

---
+++ casoTestigo/version7/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+#include "../include/tads/Coll.hpp"
+#include "../include/tads/testTools.hpp"
+#include "funciones/files.hpp"
+// #include "../funciones/strings.hpp"
+// #include "../funciones/tokens.hpp"
+// #include "../tads/Coll.hpp"
+using namespace std;
+
+// Se pide mostrar un listado indicando, por cada asignatura, 
+// el maestro a cargo, y la lista de estudiantes cuya calificación 
+// es inferior a 4 (cuatro).
+
+// Asignatura   Maestro         Estudiantes aplazados
+// Arte         Juan            2, 6, 9, 10, 15
+// Geografía    Pedro           1, 2, 8, 14
+// Historia     Juan            3, 5, 6, 10, 17, 23
+// Matemáticas  Marta           2, 4, 5, 14, 15, 16, 19, 21, 23
+// : :               :                       :
+
+
+
+int main()
+{   
+
+    return 0;
+}
+
