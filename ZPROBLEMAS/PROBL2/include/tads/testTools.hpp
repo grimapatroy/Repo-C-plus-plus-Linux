92af5e938fac0542a52c4734f23a8c32f7c46455
grimapaj <lenin.coste@gmail.com>
2023-02-01 18:10:52 -0500

    Ejercicio2

--- ZPROBLEMAS/PROBL2/include/tads/testTools.hpp
+++ ZPROBLEMAS/PROBL2/include/tads/testTools.hpp
@@ -19,318 +19,548 @@
 
 using namespace std;
 
-// string asignaturaToString(Asignatura x){
-// 	string sNomAsig=x.nomAsig;
-// 	string sMaestroACargo=x.maestroACargo;
-// 	return intToString(x.idAsig)+','+ sNomAsig +','+ sMaestroACargo + ',' + doubleToString(x.califProm);
-// }
-
-// Asignatura asignaturaFromString(string s)
-// {
-// 	Asignatura x;
-// 	x.idAsig = stringToInt(getTokenAt(s, ',', 0));
-// 	string sNomAsig = getTokenAt(s, ',', 1);
-// 	strcpy(x.nomAsig,sNomAsig.c_str());
-// 	string t2= getTokenAt(s, ',', 2);
-// 	strcpy(x.maestroACargo,t2.c_str());
-// 	x.califProm = stringToDouble(getTokenAt(s, ',', 3));
-// 	// x.idAsig = stringToInt(getTokenAt(s, ',', 2));
-// 	return x;
-// }
-
-struct Padron
+struct Resultado
 {
-    int idEst;
-    int dni;
-    char nombre[20];
-    char telefono[20];
-    char direccion[20];
-    int codigoPostal;
-    int fechaNacimiento;
-    int idEscuela;
-    int fechaMatriculacion;
-    int idSeguroSocial;
+	int idEq1;
+	int idEq2;
+	int codRes;
+	char estadio[20];
 };
 
-struct Idx
+struct Equipo
 {
-    int idEst;
-    Coll<int> collPos;
+	int idEq;
+	char nombre[20];
+	int puntos;
 };
 
-string padronToString(Padron x)
+struct REstadio
 {
-    char sep = 1;
-    string sIdEst=to_string(x.idEst);
-    string sDni=to_string(x.dni);
-    string sNombre=x.nombre;
-    string sTelefono=x.telefono;
-    string sDireccion=x.direccion;
-    string sCodigoPostal=to_string(x.codigoPostal);
-    string sFechaNacimiento=to_string(x.fechaNacimiento);
-    string sIdEscuela=to_string(x.idEscuela);
-    string sFechaMatriculacion=to_string(x.fechaMatriculacion);
-    string sIdSeguroSocial=to_string(x.idSeguroSocial);
-    return sIdEst+sep+sDni+sep+sNombre+sep+sTelefono+sep+sDireccion+sep+sCodigoPostal+sep+sFechaNacimiento+sep+sIdEscuela+sep+sFechaMatriculacion+sep+sIdSeguroSocial;
-}
+	string estadio[20];
+	int contPartido;
+	int contEmpate;
+};
 
-Padron padronFromString(string s)
+struct REquipo
 {
-    char sep = 1;
-    Padron x;
-    string t0 = getTokenAt(s,sep,0);
-    x.idEst=stoi(t0);
-    string t1 = getTokenAt(s,sep,1);
-    x.dni=stoi(t1);
-    string t2 = getTokenAt(s,sep,2);
-    strcpy(x.nombre,t2.c_str());
-    string t3 = getTokenAt(s,sep,3);
-    strcpy(x.telefono,t3.c_str());
-    string t4 = getTokenAt(s,sep,4);
-    strcpy(x.direccion,t4.c_str());
-    string t5 = getTokenAt(s,sep,5);
-    x.codigoPostal=stoi(t5);
-    string t6 = getTokenAt(s,sep,6);
-    x.fechaNacimiento=stoi(t6);
-    string t7 = getTokenAt(s,sep,7);
-    x.idEscuela=stoi(t7);
-    string t8 = getTokenAt(s,sep,8);
-    x.fechaMatriculacion=stoi(t8);
-    string t9 = getTokenAt(s,sep,9);
-    x.idSeguroSocial=stoi(t9);
-    return x;
-}
+	Equipo e;
+	int puntosActual;
+};
 
-string padronToDebug(Padron x)
+struct Equipos
 {
-    stringstream sout;
-    sout<< "[";
-    sout << x.idEst;
-    sout << ",";
-    sout << x.dni;
-    sout << ",";
-    sout << x.nombre;
-    sout << ",";
-    sout << x.telefono;
-    sout << ",";
-    sout << x.direccion;
-    sout << ",";
-    sout << x.codigoPostal;
-    sout << ",";
-    sout << x.fechaNacimiento;
-    sout << ",";
-    sout << x.idEscuela;
-    sout << ",";
-    sout << x.fechaMatriculacion;
-    sout << ",";
-    sout << x.idSeguroSocial;
-    sout<< "]";
-    return sout.str();
-}
+	Coll <REquipo> c;
+};
 
-string padronToDebug(string mssg,Padron x)
+struct Estadios
 {
-    stringstream sout;
-    sout<< mssg<<":[";
-    sout << x.idEst;
-    sout << ",";
-    sout << x.dni;
-    sout << ",";
-    sout << x.nombre;
-    sout << ",";
-    sout << x.telefono;
-    sout << ",";
-    sout << x.direccion;
-    sout << ",";
-    sout << x.codigoPostal;
-    sout << ",";
-    sout << x.fechaNacimiento;
-    sout << ",";
-    sout << x.idEscuela;
-    sout << ",";
-    sout << x.fechaMatriculacion;
-    sout << ",";
-    sout << x.idSeguroSocial;
-    sout<< "]";
-    return sout.str();
-}
+	Coll <REstadio> c;
+};
 
-Padron padron(int idEst,int dni,string nombre,string telefono,string direccion,int codigoPostal,int fechaNacimiento,int idEscuela,int fechaMatriculacion,int idSeguroSocial)
+string resultadoToString(Resultado x)
 {
-    Padron a;
-    a.idEst = idEst;
-    a.dni = dni;
-    strcpy(a.nombre,nombre.c_str());
-    strcpy(a.telefono,telefono.c_str());
-    strcpy(a.direccion,direccion.c_str());
-    a.codigoPostal = codigoPostal;
-    a.fechaNacimiento = fechaNacimiento;
-    a.idEscuela = idEscuela;
-    a.fechaMatriculacion = fechaMatriculacion;
-    a.idSeguroSocial = idSeguroSocial;
-    return a;
+	char sep = 1;
+	string sIdEq1=to_string(x.idEq1);
+	string sIdEq2=to_string(x.idEq2);
+	string sCodRes=to_string(x.codRes);
+	string sEstadio=x.estadio;
+	return sIdEq1+sep+sIdEq2+sep+sCodRes+sep+sEstadio;
 }
 
-bool padronEquals(Padron a,Padron b)
+Resultado resultadoFromString(string s)
 {
-    if(a.idEst!=b.idEst) return false;
-    if(a.dni!=b.dni) return false;
-    if(a.codigoPostal!=b.codigoPostal) return false;
-    if(a.fechaNacimiento!=b.fechaNacimiento) return false;
-    if(a.idEscuela!=b.idEscuela) return false;
-    if(a.fechaMatriculacion!=b.fechaMatriculacion) return false;
-    if(a.idSeguroSocial!=b.idSeguroSocial) return false;
-    return true;
+	char sep = 1;
+	Resultado x;
+	string t0 = getTokenAt(s,sep,0);
+	x.idEq1=stoi(t0);
+	string t1 = getTokenAt(s,sep,1);
+	x.idEq2=stoi(t1);
+	string t2 = getTokenAt(s,sep,2);
+	x.codRes=stoi(t2);
+	string t3 = getTokenAt(s,sep,3);
+	strcpy(x.estadio,t3.c_str());
+	return x;
 }
 
-string idxToString(Idx x)
+string resultadoToDebug(Resultado x)
 {
-    // char sep = 2;
-    string sIdEst=to_string(x.idEst);
-    string sCollPos=collToString<int>(x.collPos);
-    return sIdEst+"*"+sCollPos;
+	stringstream sout;
+	sout<< "[";
+	sout << x.idEq1;
+	sout << ",";
+	sout << x.idEq2;
+	sout << ",";
+	sout << x.codRes;
+	sout << ",";
+	sout << x.estadio;
+	sout<< "]";
+	return sout.str();
 }
 
-Idx idxFromString(string s)
+string resultadoToDebug(string mssg,Resultado x)
 {
-    // char sep = 2;
-    Idx x;
-    string t0 = getTokenAt(s,'*',0);
-    x.idEst=stoi(t0);
-    string t1 = getTokenAt(s,'*',1);
-    x.collPos=collFromString<int>(t1);
-    return x;
+	stringstream sout;
+	sout<< mssg<<":[";
+	sout << x.idEq1;
+	sout << ",";
+	sout << x.idEq2;
+	sout << ",";
+	sout << x.codRes;
+	sout << ",";
+	sout << x.estadio;
+	sout<< "]";
+	return sout.str();
 }
-// Idx idxFromString(string s)
-// {
-//     char sep = 2;
-//     Idx x;
-//     string t0 = getTokenAt(s,sep,0);
-//     x.idEst=stoi(t0);
-//     string t1 = getTokenAt(s,sep,1);
-//     x.collPos=collFromString<int>(t1);
-//     return x;
-// }
 
-
-
-
-string idxToDebug(Idx x)
+Resultado resultado(int idEq1,int idEq2,int codRes,string estadio)
 {
-    stringstream sout;
-    sout<< "[";
-    sout << x.idEst;
-    sout << ",";
-    collReset<int>(x.collPos);
-    int n=collSize<int>(x.collPos);
-    sout << "{";
-    for(int i=0; i<n; i++)
-    {
-        int q = collGetAt<int>(x.collPos,i,stringToInt);
-        sout << intToString(q) << ((i<n-1)?",":"");
-    }
-    sout << "}";
-    sout<< "]";
-    return sout.str();
+	Resultado a;
+	a.idEq1 = idEq1;
+	a.idEq2 = idEq2;
+	a.codRes = codRes;
+	strcpy(a.estadio,estadio.c_str());
+	return a;
 }
 
-string idxToDebug(string mssg,Idx x)
+bool resultadoEquals(Resultado a,Resultado b)
 {
-    stringstream sout;
-    sout<< mssg<<":[";
-    sout << x.idEst;
-    sout << ",";
-    collReset<int>(x.collPos);
-    int n=collSize<int>(x.collPos);
-    sout << "{";
-    for(int i=0; i<n; i++)
-    {
-        int q = collGetAt<int>(x.collPos,i,stringToInt);
-        sout << intToString(q) << ((i<n-1)?",":"");
-    }
-    sout << "}";
-    sout<< "]";
-    return sout.str();
+	if(a.idEq1!=b.idEq1) return false;
+	if(a.idEq2!=b.idEq2) return false;
+	if(a.codRes!=b.codRes) return false;
+	return true;
 }
 
-Idx idx(int idEst,Coll<int> collPos)
+string equipoToString(Equipo x)
 {
-    Idx a;
-    a.idEst = idEst;
-    a.collPos = collPos;
-    return a;
+	char sep = 2;
+	string sIdEq=to_string(x.idEq);
+	string sNombre=x.nombre;
+	string sPuntos=to_string(x.puntos);
+	return sIdEq+sep+sNombre+sep+sPuntos;
 }
 
-bool idxEquals(Idx a,Idx b)
+Equipo equipoFromString(string s)
 {
-    if(a.idEst!=b.idEst) return false;
-    if(collToString<int>(a.collPos)!=collToString<int>(b.collPos)) return false;
-    return true;
+	char sep = 2;
+	Equipo x;
+	string t0 = getTokenAt(s,sep,0);
+	x.idEq=stoi(t0);
+	string t1 = getTokenAt(s,sep,1);
+	strcpy(x.nombre,t1.c_str());
+	string t2 = getTokenAt(s,sep,2);
+	x.puntos=stoi(t2);
+	return x;
 }
 
-int cmpIdEst(Idx a ,Idx b){
-    return a.idEst-b.idEst;
+string equipoToDebug(Equipo x)
+{
+	stringstream sout;
+	sout<< "[";
+	sout << x.idEq;
+	sout << ",";
+	sout << x.nombre;
+	sout << ",";
+	sout << x.puntos;
+	sout<< "]";
+	return sout.str();
+}
+
+string equipoToDebug(string mssg,Equipo x)
+{
+	stringstream sout;
+	sout<< mssg<<":[";
+	sout << x.idEq;
+	sout << ",";
+	sout << x.nombre;
+	sout << ",";
+	sout << x.puntos;
+	sout<< "]";
+	return sout.str();
+}
+
+Equipo equipo(int idEq,string nombre,int puntos)
+{
+	Equipo a;
+	a.idEq = idEq;
+	strcpy(a.nombre,nombre.c_str());
+	a.puntos = puntos;
+	return a;
+}
+
+bool equipoEquals(Equipo a,Equipo b)
+{
+	if(a.idEq!=b.idEq) return false;
+	if(a.puntos!=b.puntos) return false;
+	return true;
+}
+
+string rEstadioToString(REstadio x)
+{
+	char sep = 3;
+	string sEstadio=x.estadio;
+	string sContPartido=to_string(x.contPartido);
+	string sContEmpate=to_string(x.contEmpate);
+	return sEstadio+sep+sContPartido+sep+sContEmpate;
+}
+
+REstadio rEstadioFromString(string s)
+{
+	char sep = 3;
+	REstadio x;
+	string t0 = getTokenAt(s,sep,0);
+	strcpy(x.estadio,t0.c_str());
+	string t1 = getTokenAt(s,sep,1);
+	x.contPartido=stoi(t1);
+	string t2 = getTokenAt(s,sep,2);
+	x.contEmpate=stoi(t2);
+	return x;
+}
+
+string rEstadioToDebug(REstadio x)
+{
+	stringstream sout;
+	sout<< "[";
+	sout << x.estadio;
+	sout << ",";
+	sout << x.contPartido;
+	sout << ",";
+	sout << x.contEmpate;
+	sout<< "]";
+	return sout.str();
+}
+
+string rEstadioToDebug(string mssg,REstadio x)
+{
+	stringstream sout;
+	sout<< mssg<<":[";
+	sout << x.estadio;
+	sout << ",";
+	sout << x.contPartido;
+	sout << ",";
+	sout << x.contEmpate;
+	sout<< "]";
+	return sout.str();
+}
+
+REstadio rEstadio(string estadio,int contPartido,int contEmpate)
+{
+	REstadio a;
+	strcpy(a.estadio,estadio.c_str());
+	a.contPartido = contPartido;
+	a.contEmpate = contEmpate;
+	return a;
+}
+
+bool rEstadioEquals(REstadio a,REstadio b)
+{
+	if(a.contPartido!=b.contPartido) return false;
+	if(a.contEmpate!=b.contEmpate) return false;
+	return true;
+}
+
+string rEquipoToString(REquipo x)
+{
+	char sep = 4;
+	string sE=equipoToString(x.e);
+	string sPuntosActual=to_string(x.puntosActual);
+	return sE+sep+sPuntosActual;
+}
+
+REquipo rEquipoFromString(string s)
+{
+	char sep = 4;
+	REquipo x;
+	string t0 = getTokenAt(s,sep,0);
+	x.e=equipoFromString(t0);
+	string t1 = getTokenAt(s,sep,1);
+	x.puntosActual=stoi(t1);
+	return x;
+}
+
+string rEquipoToDebug(REquipo x)
+{
+	stringstream sout;
+	sout<< "[";
+	sout << equipoToDebug(x.e);
+	sout << ",";
+	sout << x.puntosActual;
+	sout<< "]";
+	return sout.str();
+}
+
+string rEquipoToDebug(string mssg,REquipo x)
+{
+	stringstream sout;
+	sout<< mssg<<":[";
+	sout << equipoToDebug(x.e);
+	sout << ",";
+	sout << x.puntosActual;
+	sout<< "]";
+	return sout.str();
+}
+
+REquipo rEquipo(Equipo e,int puntosActual)
+{
+	REquipo a;
+	a.e = e;
+	a.puntosActual = puntosActual;
+	return a;
+}
+
+bool rEquipoEquals(REquipo a,REquipo b)
+{
+	if(!equipoEquals(a.e,b.e)) return false;
+	if(a.puntosActual!=b.puntosActual) return false;
+	return true;
+}
+
+string equiposToString(Equipos x)
+{
+	char sep = 5;
+	string sC=collToString<REquipo>(x.c);
+	return sC;
+}
+
+Equipos equiposFromString(string s)
+{
+	char sep = 5;
+	Equipos x;
+	string t0 = getTokenAt(s,sep,0);
+	x.c=collFromString<REquipo>(t0);
+	return x;
+}
+
+string equiposToDebug(Equipos x)
+{
+	stringstream sout;
+	sout<< "[";
+	collReset<REquipo>(x.c);
+	int n=collSize<REquipo>(x.c);
+	sout << "{";
+	for(int i=0; i<n; i++)
+	{
+		REquipo q = collGetAt<REquipo>(x.c,i,rEquipoFromString);
+		sout << rEquipoToDebug(q) << ((i<n-1)?",":"");
+	}
+	sout << "}";
+	sout<< "]";
+	return sout.str();
+}
+
+string equiposToDebug(string mssg,Equipos x)
+{
+	stringstream sout;
+	sout<< mssg<<":[";
+	collReset<REquipo>(x.c);
+	int n=collSize<REquipo>(x.c);
+	sout << "{";
+	for(int i=0; i<n; i++)
+	{
+		REquipo q = collGetAt<REquipo>(x.c,i,rEquipoFromString);
+		sout << rEquipoToDebug(q) << ((i<n-1)?",":"");
+	}
+	sout << "}";
+	sout<< "]";
+	return sout.str();
+}
+
+Equipos equipos(Coll <REquipo> c)
+{
+	Equipos a;
+	a.c = c;
+	return a;
+}
+
+bool equiposEquals(Equipos a,Equipos b)
+{
+	if(collToString<REquipo>(a.c)!=collToString<REquipo>(b.c)) return false;
+	return true;
+}
+
+string estadiosToString(Estadios x)
+{
+	char sep = 6;
+	string sC=collToString<REstadio>(x.c);
+	return sC;
+}
+
+Estadios estadiosFromString(string s)
+{
+	char sep = 6;
+	Estadios x;
+	string t0 = getTokenAt(s,sep,0);
+	x.c=collFromString<REstadio>(t0);
+	return x;
+}
+
+string estadiosToDebug(Estadios x)
+{
+	stringstream sout;
+	sout<< "[";
+	collReset<REstadio>(x.c);
+	int n=collSize<REstadio>(x.c);
+	sout << "{";
+	for(int i=0; i<n; i++)
+	{
+		REstadio q = collGetAt<REstadio>(x.c,i,rEstadioFromString);
+		sout << rEstadioToDebug(q) << ((i<n-1)?",":"");
+	}
+	sout << "}";
+	sout<< "]";
+	return sout.str();
+}
+
+string estadiosToDebug(string mssg,Estadios x)
+{
+	stringstream sout;
+	sout<< mssg<<":[";
+	collReset<REstadio>(x.c);
+	int n=collSize<REstadio>(x.c);
+	sout << "{";
+	for(int i=0; i<n; i++)
+	{
+		REstadio q = collGetAt<REstadio>(x.c,i,rEstadioFromString);
+		sout << rEstadioToDebug(q) << ((i<n-1)?",":"");
+	}
+	sout << "}";
+	sout<< "]";
+	return sout.str();
+}
+
+Estadios estadios(Coll <REstadio> c)
+{
+	Estadios a;
+	a.c = c;
+	return a;
+}
+
+bool estadiosEquals(Estadios a,Estadios b)
+{
+	if(collToString<REstadio>(a.c)!=collToString<REstadio>(b.c)) return false;
+	return true;
 }
 
 
-int cmpIdxIdEst(Idx i , int id){
-    return i.idEst-id;
-}   
-
-
-int descubirEnIdx(int idEst,Coll<Idx> collIdx){
-    int pos = collFind<Idx,int>(collIdx,idEst,cmpIdxIdEst,idxFromString);
-    if (pos<0)
-    {   
-        Idx elemIdx = idx(idEst,coll<int>(','));
-        pos = collAdd<Idx>(collIdx,elemIdx,idxToString);
-    }
-    return pos;
+Resultado resultadoLeer(FILE* archivoResul){
+	Resultado r = read<Resultado>(archivoResul);
+	return	r;
 }
 
-void mostrarLista1(Coll<Idx> collIdx,FILE* archPradron){
-    
-    collReset<Idx>(collIdx);
-    bool enc;
-    cout<<"\t"<<"ID ESTUDIANTE"<<"\t"<<"NOMBRE"<<"\t"<<"TELEFONO"<<"\t"<<"\t"<<""<<endl;
-    while (!enc)
-    {
-        Idx idx = collNext<Idx>(collIdx,enc,idxFromString);
-        if (collSize<int>(idx.collPos)<=1)
-        {
-            int posArch = collGetAt<int>(idx.collPos,0,stringToInt);
-            
-            seek<Padron>(archPradron,posArch);
-            Padron p = read<Padron>(archPradron);
-            // cout<<p.idEst<<"\t"<<p.nombre<<"\t"<<p.telefono<<endl;
-            cout<<padronToString(p)<<endl;
-        }
-    }
+Equipos  equiposCreate(){
+	FILE* archEqp = fopen("EQUIPOS.dat","r+b");
+	Equipo e = read<Equipo>(archEqp);
+	Equipos eqs = equipos(coll<REquipo>());
+	
+	while (!feof(archEqp))
+	{
+		// REquipo re = rEquipo(e,0);
+		// collAdd<REquipo>(eqs.c,re,rEquipoToString);
+		int pos = equiposBuscar(e,eqs.c);
+		if (pos<0)
+		{
+			equiposAgregar(e,eqs.c);
+		}
+		e = read<Equipo>(archEqp);
+	}
+	fclose(archEqp);
+	return eqs;
+}
+
+int cmpREquipoId(REquipo e, int id){
+	return e.e.idEq-id;
+}
+
+Estadios  estadiosCreate(){
+	Estadios est = estadios(coll<REstadio>());
+	return est;
+}
+
+int equiposObtenerPos(int idEq1,Equipos& eqs){
+	int pos = collFind<REquipo,int>(eqs.c,idEq1,cmpREquipoId,rEquipoFromString);
+	return pos;
+}
+
+REquipo equiposObtenerREquipo(int pos, Equipos& eqs){
+	REquipo re = collGetAt<REquipo>(eqs.c,pos,rEquipoFromString);
+	return	re;
+}
+
+void equiposActualizar(Equipos& eqs, REquipo& re , int pos){
+	collSetAt<REquipo>(eqs.c,re,pos,rEquipoToString);
+}
+
+void equiposGanador (int idEq1,Equipos& eqs){
+	int pos = equiposObtenerPos(idEq1,eqs);
+	REquipo re =equiposObtenerREquipo(pos,eqs);
+	re.puntosActual+=3;
+	equiposActualizar(eqs,re,pos);
 }
 
 
-void mostrarLista2(Coll<Idx> collIdx,FILE* archPradron){
-    
-    collReset<Idx>(collIdx);
-    while (collHasNext<Idx>(collIdx))
-    {
-        Idx idx = collNext<Idx>(collIdx,idxFromString);
-        if (collSize<int>(idx.collPos)>1)
-        {
-            cout<<"ID ESTUDIANTE: "<<idx.idEst<<endl;
-            // cout<<"\t"<<"NOMBRE"<<"\t"<<"DNI"<<"\t"<<"TELEFONO"<<"\t"<<"DIRECCION""\t"<<"\t"<<""<<endl;
-            collReset<int>(idx.collPos);
-            bool enc;
-            while (!enc)
-            {
-                int posArch = collNext<int>(idx.collPos,enc,stringToInt);
-                seek<int>(archPradron,posArch);
-                Padron p = read<Padron>(archPradron);
-                cout<<padronToString(p)<<endl;
-                // cout<<"\t"<<p.nombre<<"\t"<<p.dni<<"\t"<<p.telefono<<"\t"<<p.direccion<<"\t"<<endl;
-            }
-        }
-    }
+void equiposEmpate( int idEq1,int idEq2,Equipos& eqs){
+	int pos1 =equiposObtenerPos(idEq1,eqs);
+	REquipo re = equiposObtenerREquipo(pos1,eqs);
+	re.puntosActual+=1;
+	equiposActualizar(eqs,re,pos1);
+	int pos2 = equiposObtenerPos(idEq2,eqs);
+	REquipo re = equiposObtenerREquipo(pos2,eqs);
+	re.puntosActual+=1;
+	equiposActualizar(eqs,re,pos2);
 }
+
+void estadiosJugados(Estadios& estads,Resultado r ){
+	int pos = estadiosBuscar(estads,r.estadio);
+	if (pos<0)
+	{
+		// REstadio res = 
+		estadiosAgregarYContarParti(estads,r.estadio);
+	}else{
+		estadiosActualizarContParti(pos,estads);
+	}
+}
+
+
+
+void estadiosEmpate(Resultado r,Estadios& estads){
+	int pos = estadiosBuscar(estads,r.estadio);
+	if (pos<0)
+	{
+		// REstadio res = 
+		estadiosAgregarYContarParti(estads,r.estadio);
+	}else{
+		estadiosActualizarContParti(pos,estads);
+	}
+}
+
+int cmpREPuntos (REquipo e1, REquipo e2){
+	return e1.e.puntos-e2.e.puntos;
+}
+
+void equiposMostrarPoscisiones(Equipos& eqs){
+	
+	collReset<REquipo>(eqs.c);
+	for (int i = 0; i < collSize<REquipo>(eqs.c); i++)
+	{
+		REquipo re = equiposObtenerREquipo(i,eqs);
+		re.e.puntos+=re.puntosActual;
+		equiposActualizar(eqs,re,i);
+	}
+	collSort<REquipo>(eqs.c,cmpREPuntos,rEquipoFromString,rEquipoToString);
+	collReset<REquipo>(eqs.c);
+	for (int i = 0; i < collSize<REquipo>(eqs.c); i++)
+	{
+		REquipo re = equiposObtenerREquipo(i,eqs);
+		cout<<"TABLA ACTUALIZADA DE PUNTUACION "<<endl;
+		cout<<"EQUIPO"<<"\t"<<"\t"<<"\t"<<"PUNTOS"<<"\t"<<"\t"<<endl;
+		cout<<re.e.nombre<<"\t"<<"\t"<<"\t"<<re.e.puntos<<"\t"<<"\t"<<endl;
+	}	
+}
+
+void estadiosImprimir(Estadios estads){
+	cout<<"PARTIDOS JUGADOS"<<endl;
+
+	cout<<"PARTIDOS EMPATADOS"<<endl;
+}
+
 
 #endif
--- ZPROBLEMAS/PROBL2/src/main.cpp
+++ ZPROBLEMAS/PROBL2/src/main.cpp
@@ -14,7 +14,47 @@ using namespace std;
 
 int main()
 {
-    
+    FILE* archivoResul = fopen("RESULTADOS.dat","r+b");
+    Resultado r = resultadoLeer(archivoResul);
+
+    Equipos eqs = equiposCreate();
+    Estadios estads = estadiosCreate();
+
+
+    while (!feof(archivoResul))
+    {
+        if (r.codRes<0)
+        {
+                // gano equ1
+            equiposGanador(r.idEq1,eqs);
+            estadiosJugados(estads,r);
+        }
+        else
+        {
+            if (r.codRes>0)
+            {
+                // gano eq2
+                equiposGanador(r.idEq2,eqs);
+                estadiosJugados(estads,r);
+            }
+            else
+            {
+                // empate
+                equiposEmpate(r.idEq1,r.idEq2,eqs);
+                estadiosEmpate(r,estads);
+                // estadiosJugados(eqs);
+            }
+        }
+        // falta descubrir los estadios donde se jugo 
+        r = resultadoLeer(archivoResul);
+    }
+
+    equiposMostrarPoscisiones(eqs);
+    estadiosImprimir(estads);    
+    // equiposArchivoNuevosPuntos(eqs);    
+
+    fclose(archivoResul);
+
     return 0;
 }
 
